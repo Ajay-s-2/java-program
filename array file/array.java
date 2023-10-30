aa7f7c5c3088da2c3458e9a33fdc4fc6361180f6
Ajay-s-2 <sajayajay874@gmail.com>
2023-10-30 22:00:20 +0530

    the array programfile

--- array file/array.java
+++ array file/array.java
@@ -21,5 +21,23 @@ public class array {
         for (int i=0;i<rol.length;i++){
             System.out.println(rol[i]);
         }
+         for (int i=0;i<rol.length;i++){
+            System.out.println (Arrays.toString (rol));
+        }
     }
 }
+/*
+ * // input using for loops
+for (int i = 0; i < arr.length; i++) {
+arr[i] = in.nextInt ();
+}|
+for (int i = 0; i < arr.length; i++) {
+System.out.print(arr[i] + " ");
+}
+for (int num: arr) { // for every element in array, print the element
+System.out.print(num + ""); // here num represents element of the array
+}
+System.out.println(arr[5]); // index out of bound error
+
+
+ */
